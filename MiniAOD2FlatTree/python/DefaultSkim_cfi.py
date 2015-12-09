f3f74da22bc65a42b21ee332a73b2384e714355c
Alexandros Attikis <attikis@cern.ch>
2015-12-09 17:41:40 +0100

    [New Files]: Added new producer (Skim) and new analyzer (PUInfo). Applied changes to pset_cfg.py file to accomodate new changes. Need to have a look at the files PUInfo.cc  AnalysisSkim.cc  EventCounterProducer.cc and DefaultSkim_cfi.py PUInfo_cfi.py. Added new missing file

---
+++ MiniAOD2FlatTree/python/DefaultSkim_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+skim = cms.EDFilter("AnalysisSkim",
+    TriggerResults = cms.InputTag("TriggerResults::HLT"),
+    HLTPaths       = cms.vstring(
+	"HLT_LooseIsoPFTau50_Trk30_eta2p1_MET80_v",
+        "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET80_JetIdCleaned_v",
+        "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET120_v",
+        "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET120_JetIdCleaned_v",
+    ),
+    JetCollection  = cms.InputTag("slimmedJets"),
+    JetUserFloats  = cms.vstring(
+	"pileupJetId:fullDiscriminant",
+    ),
+    JetEtCut       = cms.double(20),
+    JetEtaCut      = cms.double(2.4),
+    NJets          = cms.int32(4),
+)
